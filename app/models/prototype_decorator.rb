298185875bf0d32ef457c2e6e3aa6073e0647c79
humancopy <info@humancopy.net>
2011-05-15 01:08:26 +0200

    Fix prototype model - don't override it, decorate it :)

--- app/models/prototype_decorator.rb
+++ app/models/prototype_decorator.rb
@@ -1,6 +1,4 @@
-class Prototype < ActiveRecord::Base
-  has_and_belongs_to_many :properties
+Prototype.class_eval do
   has_and_belongs_to_many :option_types, :order => "option_types_prototypes.position ASC"
   has_many :option_types_prototypes
-  validates_presence_of :name
 end
