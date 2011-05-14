aa41aa024d41563fe7ae24a162cfebabe305e246
humancopy <info@humancopy.net>
2011-05-14 12:40:50 +0200

    Replace deprecated Rails::Application.routes with Rails.application.routes
    Copied from: https://github.com/romul/spree-enhanced-option-types/commit/12e2f953f31485448dfa68a2057586768bf7cfbf

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-Rails::Application.routes.draw do |map|
+Rails.application.routes.draw do |map|
   post "/admin/products/:product_id/variants/regenerate", :to => "admin/variants#regenerate"
   put "/admin/products/:product_id/variants/inline_update", :to => "admin/variants#inline_update"
 end
