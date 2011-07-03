2cb8b56fc1bdc2eee3e6748c637e6618471c0e32
humancopy <info@humancopy.net>
2011-07-03 18:47:38 +0200

    Update hooks for Spree 0.60

--- lib/spree_enhanced_option_types_hooks.rb
+++ lib/spree_enhanced_option_types_hooks.rb
@@ -1,4 +1,17 @@
 class SpreeEnhancedOptionTypesHooks < Spree::ThemeSupport::HookListener
-  insert_after :admin_product_form_right, :partial => 'admin/products/extra_master_values_form'
-  insert_after :admin_inside_head, :partial => 'admin/shared/jeditable'
+  Deface::Override.new(:virtual_path => "admin/product_scopes/_form",
+                       :name => "enhanced_option_types_admin_product_form_right",
+                       :insert_after => "[data-hook='admin_product_form_right'], #admin_product_form_right[data-hook]",
+                       :partial => "admin/products/extra_master_values_form",
+                       :disabled => false)
+
+  Deface::Override.new(:virtual_path => "layouts/admin",
+                       :name => "enhanced_option_types_admin_inside_head",
+                       :insert_after => "[data-hook='admin_inside_head'], #admin_inside_head[data-hook]",
+                       :partial => "admin/shared/jeditable",
+                       :disabled => false)
+
+
+  # insert_after :admin_product_form_right, :partial => 'admin/products/extra_master_values_form'
+  # insert_after :admin_inside_head, :partial => 'admin/shared/jeditable'
 end
