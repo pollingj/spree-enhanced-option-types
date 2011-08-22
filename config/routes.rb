bb461c0409478794a671774252b92ee120c56414
humancopy <info@humancopy.net>
2011-08-22 02:00:49 +0200

    Add a name and remove parameter from block

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-Rails.application.routes.draw do |map|
-  match '/admin/products/:product_id/variants/regenerate' => 'admin/variants#regenerate', :via => 'post'
-  match '/admin/products/:product_id/variants/inline_update' => 'admin/variants#inline_update', :via => 'put'
+Rails.application.routes.draw do
+  match '/admin/products/:product_id/variants/regenerate' => 'admin/variants#regenerate', :via => :post, :as => :admin_product_variants_regenerate
+  match '/admin/products/:product_id/variants/inline_update' => 'admin/variants#inline_update', :via => :put, :as => :admin_product_variants_inline_update
 end
