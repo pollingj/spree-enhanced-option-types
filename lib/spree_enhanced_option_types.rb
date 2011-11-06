6aa5d2efa45baa18a0639adb35333b471cafa034
humancopy <info@humancopy.net>
2011-11-06 21:35:11 +0100

    Use notification instead of overriding OrdersController.

--- lib/spree_enhanced_option_types.rb
+++ lib/spree_enhanced_option_types.rb
@@ -1,4 +1,4 @@
 require 'spree_core'
 require 'spree_enhanced_option_types/engine'
 require 'spree_enhanced_option_types/numeric_extension'
-# require 'spree_enhanced_option_types/notifications'
+require 'spree_enhanced_option_types/notifications'
