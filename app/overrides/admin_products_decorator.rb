075c8848ad865365a320c818063f720bb62c3719
humancopy <info@humancopy.net>
2011-11-06 09:02:03 +0100

    The way Spree is now, it is not possible to deface admin/products/new so for now it's a template override.

--- app/overrides/admin_products_decorator.rb
+++ app/overrides/admin_products_decorator.rb
@@ -6,10 +6,10 @@ Deface::Override.new(
   :disabled => false
 )
 
-Deface::Override.new(
-  :virtual_path => "admin/products/_new",
-  :name => "enhanced_option_types_admin_products_new",
-  :insert_bottom => "[data-hook='new_product_attrs']",
-  :partial => "admin/products/enhanced_option_new",
-  :disabled => false
-)
+# Deface::Override.new(
+#   :virtual_path => "admin/products/_new",
+#   :name => "enhanced_option_types_admin_products_new",
+#   :insert_bottom => "[data-hook='new_product_attrs']",
+#   :partial => "admin/products/enhanced_option_new",
+#   :disabled => false
+# )
