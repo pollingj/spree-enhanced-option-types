55d955e34439feaf955cb6f67cddfbbd1f02f1b4
humancopy <info@humancopy.net>
2011-11-06 09:54:13 +0100

    Cart form decorator small bug

--- app/overrides/products_decorator.rb
+++ app/overrides/products_decorator.rb
@@ -2,6 +2,6 @@ Deface::Override.new(
   :virtual_path => "products/_cart_form",
   :name => "enhanced_option_types_products_variants",
   :replace => "#product-variants",
-  :partial => Spree::Config[:eot_presentation_type] || 'radio_sets',
+  :partial => Spree::Config[:eot_presentation_type] || 'products/radio_sets',
   :disabled => false
 )
