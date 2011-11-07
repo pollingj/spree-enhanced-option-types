493d53e259b478d9c3085b8104de821bad946869
humancopy <info@humancopy.net>
2011-11-07 21:35:33 +0100

    The extra fields didn't show up in the product edit page.

--- app/overrides/admin_products_decorator.rb
+++ app/overrides/admin_products_decorator.rb
@@ -1,7 +1,7 @@
 Deface::Override.new(
-  :virtual_path => "admin/product_scopes/_form",
+  :virtual_path => "admin/products/_form",
   :name => "enhanced_option_types_admin_product_form_right",
-  :insert_after => "[data-hook='admin_product_form_right'], #admin_product_form_right[data-hook]",
+  :insert_bottom => "[data-hook='admin_product_form_right']",
   :partial => "admin/products/extra_master_values_form",
   :disabled => false
 )
